ca3ce85b209b7d9cc3f1c066925289a3728facf1
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-03-10 00:28:26 +0300

    chore(index): import different Root component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Root } from './Root.tsx';
+
+import { Root } from './components/Root';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Root />
+    <Root/>
   </React.StrictMode>,
 );
