b8674a0af799ea54f490164bc1c0144174fc0359
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-03-09 13:46:06 +0300

    chore(vite-config): actualize vite.config.ts. Comment server option and add comment

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,25 @@
-import { readFileSync } from 'node:fs';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { defineConfig, type ServerOptions } from 'vite';
+// import { readFileSync } from 'node:fs';
+// import { dirname, resolve } from 'node:path';
+// import { fileURLToPath } from 'node:url';
+// import type { ServerOptions } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
-/**
- * Returns Vite dev server options.
- */
-function getServerOptions(): ServerOptions {
-  const dir = dirname(fileURLToPath(import.meta.url));
-
-  return {
-    port: 443,
-    https: {
-      cert: readFileSync(resolve(dir, './https-cert.pem')),
-      key: readFileSync(resolve(dir, './https-key.pem')),
-    },
-    host: 'tma.internal',
-  };
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Uncomment this line in case, you would like to run Vite dev server using HTTPS and in case,
-  // you have key and certificate.
-  server: getServerOptions(),
+  // Uncomment the next lines in case, you would like to run Vite dev server using HTTPS and in case,
+  // you have key and certificate. You retrieve your certificate and key using mkcert.
+  // Learn more:
+  // https://docs.telegram-mini-apps.com/platform/getting-app-link#mkcert
+  //
+  // server: {
+  //   port: 443,
+  //   https: {
+  //     cert: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './https-cert.pem')),
+  //     key: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './https-key.pem')),
+  //   },
+  //   host: 'tma.internal',
+  // }
 });
+
