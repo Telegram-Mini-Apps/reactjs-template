b09d53e28d58ebfbf6c5701c9a5922233d9b1035
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-03-31 19:35:24 +0300

    refactor(components): move RGB component outside of DataTable

---
+++ src/components/RGB/RGB.tsx
@@ -0,0 +1,24 @@
+import type { RGB as RGBType } from '@tma.js/sdk';
+import { classNames } from '@tma.js/sdk';
+import type { FC } from 'react';
+
+import './RGB.css';
+
+export type RGBProps = JSX.IntrinsicElements['div'] & {
+  color: RGBType;
+};
+
+export const RGB: FC<RGBProps> = (props) => {
+  const {
+    color,
+    className,
+    ...rest
+  } = props;
+
+  return (
+    <span {...rest} className={classNames('rgb', className)}>
+      <i className="rgb__icon" style={{ backgroundColor: color }} />
+      {color}
+    </span>
+  );
+};
