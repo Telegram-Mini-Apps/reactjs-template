42eb4216a08d4865d36b9fe131b5a043414a3b9d
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-05-22 01:13:22 +0300

    Actualize the template

--- src/pages/InitDataPage/InitDataPage.tsx
+++ src/pages/InitDataPage/InitDataPage.tsx
@@ -10,11 +10,15 @@ import './InitDataPage.css';
 function getUserRows(user: User): DisplayDataRow[] {
   return [
     { title: 'id', value: user.id.toString() },
+    { title: 'username', value: user.username },
+    { title: 'photo_url', value: user.photoUrl },
     { title: 'last_name', value: user.lastName },
     { title: 'first_name', value: user.firstName },
     { title: 'is_bot', value: user.isBot },
     { title: 'is_premium', value: user.isPremium },
     { title: 'language_code', value: user.languageCode },
+    { title: 'allows_to_write_to_pm', value: user.allowsWriteToPm },
+    { title: 'added_to_attachment_menu', value: user.addedToAttachmentMenu },
   ];
 }
 
@@ -116,7 +120,7 @@ export const InitDataPage: FC = () => {
         <>
           This page displays application
           {' '}
-          <Link to="https://docs.telegram-mini-apps.com/platform/launch-parameters">
+          <Link to="https://docs.telegram-mini-apps.com/platform/init-data">
             init data
           </Link>
           .
