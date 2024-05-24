390d7c7e8c6cc948833aab1d73178ae5a73750b8
Gleb Vorontsov <vorontsov.gleb@gmail.com>
2024-05-24 15:17:37 +0200

    SSL plugin for vercel, fix for base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
-  plugins: [react(), tsconfigPaths()],
+  base: '/reactjs-template',
+  plugins: [react(), tsconfigPaths(), basicSsl()],
   // Uncomment the next lines in case, you would like to run Vite dev server using HTTPS and in case,
   // you have key and certificate. You retrieve your certificate and key using mkcert.
   // Learn more:
