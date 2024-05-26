01e9329ae61c1823eb51c78754128879ffa39d5d
qbnk <wolfram.deus@gmail.com>
2024-05-26 14:05:29 +0300

    Refactor config file in context of HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,43 @@
 // import { readFileSync } from 'node:fs';
-// import { dirname, resolve } from 'node:path';
+// import { dirname } from 'node:path';
 // import { fileURLToPath } from 'node:url';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/reactjs-template',
-  plugins: [react(), tsconfigPaths(), basicSsl()],
-  // Uncomment the next lines in case, you would like to run Vite dev server using HTTPS and in case,
-  // you have key and certificate. You retrieve your certificate and key using mkcert.
-  // Learn more:
-  // https://docs.telegram-mini-apps.com/platform/getting-app-link#mkcert
-  //
-  // server: {
-  //   port: 443,
-  //   https: {
-  //     cert: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './tma.internal.pem')),
-  //     key: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './tma.internal-key.pem')),
-  //   },
-  //   host: 'tma.internal',
-  // },
+  plugins: [
+    // Allows using React dev server along with building a React application with Vite.
+    // https://npmjs.com/package/@vitejs/plugin-react-swc
+    react(),
+    // Allows using the compilerOptions.paths property in tsconfig.json.
+    // https://www.npmjs.com/package/vite-tsconfig-paths
+    tsconfigPaths(),
+    // Allows using self-signed certificates to run the dev server using HTTPS.
+    // In case, you have a trusted SSL certificate with a key, you could use the server.https
+    // option not to proceed to the untrusted SSL certificate warning.
+    // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
+    basicSsl({
+      certDir: resolve('certificates'),
+      domains: ['tma.internal'],
+    }),
+  ],
+  server: {
+    port: 443,
+    host: 'tma.internal',
+    // Uncomment the next lines in case, you would like to run Vite dev server using HTTPS and in
+    // case, you have trusted key and certificate. You retrieve your certificate and key
+    // using mkcert.
+    // Learn more: https://docs.telegram-mini-apps.com/platform/getting-app-link#mkcert
+    // https: {
+    //   cert: readFileSync(resolve('certificates/tma.internal.pem')),
+    //   key: readFileSync(resolve('certificates/tma.internal-key.pem')),
+    // },
+  },
   publicDir: './public',
 });
 
