b85cef806e857a325fc887e96fb09275f1689260
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-06-09 23:55:10 +0300

    Update mock console notification

--- src/mockEnv.ts
+++ src/mockEnv.ts
@@ -62,7 +62,7 @@ if (import.meta.env.DEV) {
     sessionStorage.setItem('____mocked', '1');
 
     console.info(
-      'As long as the current environment was not considered as the Telegram-based one, it was mocked. Take a note, that you should not do it in production and current behavior is only specific to the development process. Environment mocking is also applied only in development mode. So, after building the application, you will not see this behavior and related warning, leading to crashing the application outside Telegram.',
+      'As long as the current environment was not considered as the Telegram-based one, it was mocked. Take a note, that you should not do it in production and current behavior is only specific to the development process. Environment mocking is also applied only in development mode. So, after building the application, you will not see this behavior and related warning, leading to crashing the application outside Telegram.\n\nTo learn more, refer the to the project\'s README.md file.',
     );
   }
 }
