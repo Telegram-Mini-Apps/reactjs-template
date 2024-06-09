bc78831d3c54d5aaac6bdfe6bebf6acbaae5cb6d
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-06-09 23:53:08 +0300

    Add mock console notification

--- src/mockEnv.ts
+++ src/mockEnv.ts
@@ -4,12 +4,23 @@ import { mockTelegramEnv, parseInitData, retrieveLaunchParams } from '@tma.js/sd
 // application, import.meta.env.DEV will become false, and the code inside will be tree-shaken,
 // so you will not see it in your final bundle.
 if (import.meta.env.DEV) {
-  // We are not mocking the environment in case, we are already in the Telegram app.
+  let shouldMock: boolean;
+
+  // Try to extract launch parameters to check if the current environment is Telegram-based.
   try {
     // If we are able to extract launch parameters, it means that we are already in the
     // Telegram environment. So, there is no need to mock it.
     retrieveLaunchParams();
+
+    // We could previously mock the environment. In case we did, we should do it again. The reason
+    // is the page could be reloaded, and we should apply mock again, because mocking also
+    // enables modifying the window object.
+    shouldMock = !!sessionStorage.getItem('____mocked');
   } catch (e) {
+    shouldMock = true;
+  }
+
+  if (shouldMock) {
     const initDataRaw = new URLSearchParams([
       ['user', JSON.stringify({
         id: 99281932,
@@ -48,5 +59,10 @@ if (import.meta.env.DEV) {
       version: '7.2',
       platform: 'tdesktop',
     });
+    sessionStorage.setItem('____mocked', '1');
+
+    console.info(
+      'As long as the current environment was not considered as the Telegram-based one, it was mocked. Take a note, that you should not do it in production and current behavior is only specific to the development process. Environment mocking is also applied only in development mode. So, after building the application, you will not see this behavior and related warning, leading to crashing the application outside Telegram.',
+    );
   }
 }
