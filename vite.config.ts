e5865f1203cf8da8c3c6e6a4de0bd08bc817e4ac
Amir <far20022002213@gmail.com>
2024-10-13 21:02:46 +0500

    pages feat: added editorconfig, fixed base router path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template',
+  base: '/',
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
